c0fc7791fb1625f49b0f23c8a84c4bd6b41d8e2e
joshuaPZ <joshuaprieto8@gmail.com>
2025-02-20 16:18:18 -0500

    Add: SpringBoot Estrcutura - Creacion de Carpetas y propiedades

---
+++ src/test/java/co/edu/javeriana/Caravana_Medieval/CaravanaMedievalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.Caravana_Medieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanaMedievalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
