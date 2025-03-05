25e0e30e4b35403eff2dbeba856f5f3b45d250e5
Amdres <ahenao.2005@gmail.com>
2025-03-05 13:32:50 -0500

    feat: Agregados atributos para mostrar en templates relacionados a DTOs  - Agregados listas para mostrar los nombres y textos de los atributos especicos de las relaciones que se quieren mostrar dentro de el template ciudades-list.html  - Modificadas mapper y ciudades-list.html para ajustarse a los cambios realizados

--- src/main/java/co/edu/javeriana/caravana_medieval/dto/CiudadDTO.java
+++ src/main/java/co/edu/javeriana/caravana_medieval/dto/CiudadDTO.java
@@ -1,17 +1,84 @@
 package co.edu.javeriana.caravana_medieval.dto;
 
+import java.util.List;
+
 public class CiudadDTO {
     private Long id;
     private String nombre;
     private int impuesto;
+    private List<String> productoNombres;
+    private List<String> servicioNombres;
+    private List<String> compraServicioNombres;
+    private List<String> compraCaravanaNombres;
+    private List<String> rutaOrigenNombres;
+    private List<String> rutaDestinoNombres;
 
 
     public CiudadDTO(){
     }
-    public CiudadDTO(Long id, String nombre, int impuesto) {
+
+    public CiudadDTO(Long id, String nombre, int impuesto, List<String> productoNombres, List<String> servicioNombres, List<String> compraServicioNombres, List<String> compraCaravanaNombres, List<String> rutaOrigenNombres, List<String> rutaDestinoNombres) {
         this.id = id;
         this.nombre = nombre;
         this.impuesto = impuesto;
+        this.productoNombres = productoNombres;
+        this.servicioNombres = servicioNombres;
+        this.compraServicioNombres = compraServicioNombres;
+        this.compraCaravanaNombres = compraCaravanaNombres;
+        this.rutaOrigenNombres = rutaOrigenNombres;
+        this.rutaDestinoNombres = rutaDestinoNombres;
+    }
+
+    public List<String> getRutaOrigenNombres() {
+        return rutaOrigenNombres;
+    }
+
+    public void setRutaOrigenNombres(List<String> rutaOrigenNombres) {
+        this.rutaOrigenNombres = rutaOrigenNombres;
+    }
+
+    public List<String> getRutaDestinoNombres() {
+        return rutaDestinoNombres;
+    }
+
+    public void setRutaDestinoNombres(List<String> rutaDestinoNombres) {
+        this.rutaDestinoNombres = rutaDestinoNombres;
+    }
+
+    public void setCompraServicioNombres(List<String> compraServicioNombres) {
+        this.compraServicioNombres = compraServicioNombres;
+    }
+
+    public List<String> getCompraCaravanaNombres() {
+        return compraCaravanaNombres;
+    }
+
+    public void setCompraCaravanaNombres(List<String> compraCaravanaNombres) {
+        this.compraCaravanaNombres = compraCaravanaNombres;
+    }
+
+    public List<String> getCompraServicioNombres() {
+        return compraServicioNombres;
+    }
+
+    public void setCompraServicioNombre(List<String> compraServicioNombres) {
+        this.compraServicioNombres = compraServicioNombres;
+    }
+
+    public List<String> getServicioNombres() {
+        return servicioNombres;
+    }
+
+    public void setServicioNombres(List<String> servicioNombres) {
+        this.servicioNombres = servicioNombres;
+    }
+
+    public List<String> getProductoNombres() {
+        return productoNombres;
+    }
+
+    public void setProductoNombres(List<String> productoNombres) {
+        this.productoNombres = productoNombres;
     }
 
     public Long getId() {
