0014f4a63a141b3a178db64364ff16b83644d791
ahenaon00 <ahenao.2005@gmail.com>
2025-03-09 22:57:46 -0500

    CiudadDTO

--- src/main/java/co/edu/javeriana/caravana_medieval/dto/CiudadDTO.java
+++ src/main/java/co/edu/javeriana/caravana_medieval/dto/CiudadDTO.java
@@ -1,13 +1,34 @@
 package co.edu.javeriana.caravana_medieval.dto;
 
+import java.util.List;
+
 public class CiudadDTO {
+    private Long id;
     private String nombre;
     private int impuesto;
 
-    public CiudadDTO() {
+    public CiudadDTO(){
     }
 
+    public CiudadDTO(Long id, String nombre, int impuesto, List<String> productoNombres, List<String> servicioNombres, List<String> compraServicioNombres, List<String> compraCaravanaNombres, List<String> rutaOrigenNombres, List<String> rutaDestinoNombres) {
+        this.id = id;
         this.nombre = nombre;
+        this.impuesto = impuesto;;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getImpuesto() {
+        return impuesto;
+    }
+
+    public void setImpuesto(int impuesto) {
         this.impuesto = impuesto;
     }
 
