0bb124ab18a3141412f427a980c0aa03b83cef19
joshuaPZ <joshuaprieto8@gmail.com>
2025-03-24 13:28:56 -0500

    add: En Angular service de ciudades y el front de este mismo (components)
    Se dejo inicializado para hacer ciudad-view, por el momento solo esta ciudad-list.
    
    Tambien en el backend se puso el tema de CORS y ya estaria para empezar a hacer conexiones sin problema

--- Front-Juego-Caravana-Medieval/src/app/app.component.ts
+++ Front-Juego-Caravana-Medieval/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CiudadListComponent } from './ciudad/ciudad-list/ciudad-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CiudadListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Front-Juego-Caravana-Medieval';
