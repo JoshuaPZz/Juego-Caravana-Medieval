5e53c02e3dce55d64a7b44500416bf41ca64765d
Joan Emmanuel <joan_orduz@javeriana.edu.co>
2025-03-27 18:15:13 -0500

    refactor: Migracion a Rest de CiudadServicioController
    - CRUD completo en rest de ciudadServicio

--- Juego-Caravana-Medieval/src/main/java/co/edu/javeriana/caravana_medieval/controller/CiudadServicioController.java
+++ Juego-Caravana-Medieval/src/main/java/co/edu/javeriana/caravana_medieval/controller/CiudadServicioController.java
@@ -5,18 +5,18 @@ import java.util.List;
 import co.edu.javeriana.caravana_medieval.repository.ProductoRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 import co.edu.javeriana.caravana_medieval.service.*;
 import co.edu.javeriana.caravana_medieval.dto.*;
+import co.edu.javeriana.caravana_medieval.mapper.CiudadServicioMapper;
+import co.edu.javeriana.caravana_medieval.model.Ciudad;
 
 import org.slf4j.Logger;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
-@RequestMapping("/ciudades/view/{id}")
+@RestController
+@RequestMapping("/ciudad/{id}/servicios")
 public class CiudadServicioController {
     private Logger log = LoggerFactory.getLogger(getClass().getName());
     @Autowired
@@ -24,41 +24,31 @@ public class CiudadServicioController {
     @Autowired
     private ServicioService servicioService;
 
-    @GetMapping("/servicios")
-    public ModelAndView getProductosCiudad(@PathVariable("id") Long id) {
-        CiudadDTO ciudad = ciudadService.getCiudadById(id).get();
+    @GetMapping("/list")
+    public List<ServicioDTO> getServiciosCiudad(@PathVariable("id") Long id) {
         List<CiudadServicioDTO> ciudadServiciosDTO = ciudadService.getCiudadService(id).orElseThrow();
-        List<ServicioDTO> servicios = servicioService.listaIdsToProducto(ciudadServiciosDTO.stream().map(CiudadServicioDTO::getIdServicio).toList());
-        ModelAndView modelAndView = new ModelAndView("servicio-ciudad-view");
-        modelAndView.addObject("ciudad", ciudad);
-        modelAndView.addObject("servicios", servicios);
-        return modelAndView;
+        return servicioService.listaIdsToServicio(ciudadServiciosDTO.stream().map(CiudadServicioDTO::getIdServicio).toList());
     }
 
-    @GetMapping("servicios/{idServicio}")
-    public ModelAndView getServicioCiudadTupla(@PathVariable("id") Long id, @PathVariable("idServicio") Long idServicio) {
+    @GetMapping("view/{idCiudadServicio}")
+    public CiudadServicioDTO getServicioCiudadTupla(@PathVariable("id") Long id, @PathVariable("idCiudadServicio") Long idCiudadServicio) {
         List<CiudadServicioDTO> ciudadServiciosDTO = ciudadService.getCiudadService(id).get();
-        CiudadServicioDTO ciudadServicioDTO = ciudadService.getCiudadServicioTupla(ciudadServiciosDTO, id, idServicio);
-        ModelAndView modelAndView = new ModelAndView("servicio-tupla");
-        modelAndView.addObject("ciudadServicio", ciudadServicioDTO);
-        return modelAndView;
+        return ciudadService.getCiudadServicioTupla(ciudadServiciosDTO, idCiudadServicio);
     }
 
-    @PostMapping("servicio/{idServicio}/save")
-    public RedirectView saveEditServicioCiudad(@ModelAttribute CiudadServicioDTO ciudadServicioDTO, @PathVariable("id") Long id, @PathVariable Long idServicio) {
-        ciudadServicioDTO.setIdCiudad(id);
-        ciudadServicioDTO.setIdServicio(idServicio);
-        ciudadService.saveEditServicioCiudad(ciudadServicioDTO);
-        return new RedirectView("/ciudades/view/" + id + "/servicios");
+    @PostMapping
+    public CiudadServicioDTO createServicioCiudad(@RequestBody CiudadServicioDTO ciudadServicioDTO) {
+        return CiudadServicioMapper.toDTO(ciudadService.createCiudadServicio(ciudadServicioDTO));
+    }    
+
+    @PutMapping
+    public CiudadServicioDTO updateServicioCiudad(@RequestBody CiudadServicioDTO ciudadServicioDTO) {
+        return CiudadServicioMapper.toDTO(ciudadService.updateCiudadServicio(ciudadServicioDTO));
     }
 
-    @GetMapping("servicio/{idServicio}/delete")
-    public RedirectView deleteServicioCiudad(@PathVariable("id") Long idCiudad, @PathVariable Long idServicio) {
-        log.error("id de ciudad: "+ idCiudad.toString());
-        log.error("id de servocio: "+ idServicio.toString());
-        ciudadService.borrarCiudadServicio(idCiudad, idServicio);
-        return new RedirectView("/ciudades/view/" + idCiudad + "/servicios");
+    @DeleteMapping("{idCiudadServicio}")
+    public void borrarCiudadServicio(@PathVariable("idCiudadServicio") Long idCiudadServicio){
+        ciudadService.deleteCiudadServicio(idCiudadServicio);
     }
-
 
 }
