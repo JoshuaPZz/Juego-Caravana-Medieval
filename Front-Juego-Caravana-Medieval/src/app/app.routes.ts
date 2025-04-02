8c2c537bf8f1ecbc9049265f293c80f2b1a966fd
ahenaon00 <ahenao.2005@gmail.com>
2025-04-01 19:50:13 -0500

    feat: Vista que muestra todos los productos
     -Generado el componente para mostrar todos los productos obtenidos de el servidor spring
     -Creado RestController para manejar las peticiones relacionadas a productos

--- Front-Juego-Caravana-Medieval/src/app/app.routes.ts
+++ Front-Juego-Caravana-Medieval/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CiudadListComponent } from './ciudad/ciudad-list/ciudad-list.component';
 import { CiudadViewComponent } from './ciudad/ciudad-view/ciudad-view.component';
 import { ViajarCiudadComponent } from './viaje/viajar-ciudad/viajar-ciudad.component';
+import { ProductosListComponent } from './productos/productos-list/productos-list.component';
 
 export const routes: Routes = [
   { path: 'ciudades/list', component: CiudadListComponent },
   { path: 'ciudades/view/:id', component: CiudadViewComponent },
   { path: 'viaje/otro/ciudad', component: ViajarCiudadComponent },
+  { path: 'productos/list', component: ProductosListComponent },
 ];
