3e2a8752f06b6fab178d19278dce773752266424
joshuaPZ <joshuaprieto8@gmail.com>
2025-04-10 16:34:55 -0500

    Add: query para busqueda de productos en caravana

--- Juego-Caravana-Medieval/src/main/java/co/edu/javeriana/caravana_medieval/repository/CaravanaProductoRepository.java
+++ Juego-Caravana-Medieval/src/main/java/co/edu/javeriana/caravana_medieval/repository/CaravanaProductoRepository.java
@@ -9,11 +9,19 @@ import co.edu.javeriana.caravana_medieval.model.CaravanaProducto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CaravanaProductoRepository extends JpaRepository<CaravanaProducto, Long> {
 
     @Query("SELECT cp FROM CaravanaProducto cp WHERE cp.caravana.id = :idCaravana")
     List<CaravanaProducto> findByIdCaravana(@Param("idCaravana") Long idCaravana);
+
+    @Query("SELECT cp FROM CaravanaProducto cp WHERE cp.caravana.id = :caravanaId AND cp.producto.id = :productoId")
+    Optional<CaravanaProducto> findByCaravanaIdAndProductoId(
+            @Param("caravanaId") Long caravanaId, 
+            @Param("productoId") Long productoId);
+    
+    void deleteByCaravanaId(Long caravanaId);
 
 }
