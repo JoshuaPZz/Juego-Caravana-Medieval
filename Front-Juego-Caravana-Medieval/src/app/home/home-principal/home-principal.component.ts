7231a7815bc87ae74ce1a95a26422cf766b60a1c
ahenaon00 <ahenao.2005@gmail.com>
2025-04-18 23:39:34 -0500

    feat: agregado component para acceder a acciones principales desede una caravana
    - Agregados todos los botones con sus respectivos routers para redirigir a la url correspondiente al componente deseado

--- Front-Juego-Caravana-Medieval/src/app/home/home-principal/home-principal.component.ts
+++ Front-Juego-Caravana-Medieval/src/app/home/home-principal/home-principal.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CaravanaViewComponent } from '../../caravana/caravana-view/caravana-view.component';
 
 @Component({
   selector: 'app-home-principal',
-  imports: [],
+  imports: [CaravanaViewComponent],
   templateUrl: './home-principal.component.html',
   styleUrl: './home-principal.component.css'
 })
 export class HomePrincipalComponent {
+
+  constructor(
+    private router : Router
+  ) {}
+  viajar() : void {
+    this.router.navigate(['/viajar'])
+  }
+  comprarProductos() : void {
+    this.router.navigate(['/comprar/producto'])
+  }
+  comprarServicios() : void {
+    this.router.navigate(['/comprar/servicio'])
+  }
+  venderProducto() : void {
+    this.router.navigate([''])
+  }
+  verInventario() : void {
+    this.router.navigate([''])
+  }
 
 }
