f9d92ffd9249502db4afcf2d461a0a00125b8d1f
joshuaPZ <joshuaprieto8@gmail.com>
2025-04-20 18:05:15 -0500

    addL musiquita home

--- Front-Juego-Caravana-Medieval/src/app/home/home-inicio/home-inicio.component.ts
+++ Front-Juego-Caravana-Medieval/src/app/home/home-inicio/home-inicio.component.ts
@@ -1,15 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-inicio',
+  standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './home-inicio.component.html',
   styleUrl: './home-inicio.component.css',
 })
-export class HomeInicioComponent {
+export class HomeInicioComponent implements OnInit {
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const music = document.getElementById(
+      'backgroundMusic'
+    ) as HTMLAudioElement;
+    if (music) {
+      music.volume = 0.4;
+    }
+  }
 
   startGame(): void {
     this.router.navigate(['/principal']);
