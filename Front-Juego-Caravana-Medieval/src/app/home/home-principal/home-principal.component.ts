d7434ffd9a86311e771ab8fe54b393783e38dd13
joshuaPZ <joshuaprieto8@gmail.com>
2025-04-21 15:17:41 -0500

    add: imagenes para las ciudades en la que la caravan esta

--- Front-Juego-Caravana-Medieval/src/app/home/home-principal/home-principal.component.ts
+++ Front-Juego-Caravana-Medieval/src/app/home/home-principal/home-principal.component.ts
@@ -56,4 +56,49 @@ export class HomePrincipalComponent {
   verInventario(): void {
     this.router.navigate(['/list/caravana/productos']);
   }
+
+  getCiudadImagen(): string {
+    const idsCaelid = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+    const idsAnorlondo = [2, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+    const idsClubpenguin = [3, 12, 22, 32, 42, 52, 62, 72, 82, 92];
+    const idsDeathstar = [4, 13, 23, 33, 43, 53, 63, 73, 83, 93];
+    const idsHoradeaventuras = [5, 14, 24, 34, 44, 54, 64, 74, 84, 94];
+    const idsTatooine = [6, 15, 25, 35, 45, 55, 65, 75, 85, 95];
+    const idsTorresauron = [7, 16, 26, 36, 46, 56, 66, 76, 86, 96];
+    const idsUnshowmas = [8, 17, 27, 37, 47, 57, 67, 77, 87, 97];
+    const idsZelda = [9, 18, 28, 38, 48, 58, 68, 78, 88, 98];
+    const idsMinecraft = [19, 29, 39, 49, 59, 69, 79, 89, 99, 100];
+    if (idsCaelid.includes(this.ciudadActual?.id)) {
+      return 'assets/images/caelid.png';
+    }
+    if (idsAnorlondo.includes(this.ciudadActual?.id)) {
+      return 'assets/images/anorlondo.png';
+    }
+    if (idsClubpenguin.includes(this.ciudadActual?.id)) {
+      return 'assets/images/clubpenguin.png';
+    }
+    if (idsDeathstar.includes(this.ciudadActual?.id)) {
+      return 'assets/images/deathstar.png';
+    }
+    if (idsHoradeaventuras.includes(this.ciudadActual?.id)) {
+      return 'assets/images/horadeaventura.png';
+    }
+    if (idsTatooine.includes(this.ciudadActual?.id)) {
+      return 'assets/images/tatooine.png';
+    }
+    if (idsTorresauron.includes(this.ciudadActual?.id)) {
+      return 'assets/images/torresauron.png';
+    }
+    if (idsUnshowmas.includes(this.ciudadActual?.id)) {
+      return 'assets/images/unshowmas.png';
+    }
+    if (idsZelda.includes(this.ciudadActual?.id)) {
+      return 'assets/images/zelda.png';
+    }
+    if (idsMinecraft.includes(this.ciudadActual?.id)) {
+      return 'assets/images/minecraft.png';
+    }
+
+    return '1';
+  }
 }
